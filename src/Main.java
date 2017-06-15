aef9d3be4963ac75c9c4e5bc2c243a247d828e6a
yokoyama <m.yokoyama@atomitech.jp>
2017-06-15 17:32:24 +0900

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Mainクラス
+ * Created by yokoyama on 2017/06/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello,World"   );
+    }
+}
